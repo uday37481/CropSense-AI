d0bd4ce70846c400e0142ead3977489c56fd54c7
Uday Nagar <uvnagar370823@kkwagh.edu.in>
2026-03-16 22:05:27 +0530

    Add files via upload

---
+++ frontend/dashboard/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { DashboardSummary } from './components/DashboardSummary'
+
+function App() {
+
+  return (
+    <div className="app-container">
+      <DashboardSummary />
+    </div>
+  )
+}
+
+export default App
